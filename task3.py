5b27532d079e9835c685e29d3ad2256cb0a5c2e2
alexxkrava <Kravchenkoalex94@gmail.com>
2023-02-16 22:47:03 +0300

    New_Task4

--- task3.py
+++ task3.py
@@ -1,9 +1,29 @@
+total = 0
+res = {}
+res1 = []
 for i in range(1, 4):
     doc = str(i)+'.txt'
-    with open(doc, 'r+', encoding='UTF-8') as f:
-        res = f.read()
-        with open('4.txt', 'a', encoding='UTF-8') as file:
-            file.write(doc + '\n')
-            file.write(res + '\n')
-print(file)
+    f = open(doc, 'r+', encoding = 'UTF 8')
+    for line in f:
+        total += 1
+    res[doc] = total
+    res1.append(total)
+    total = 0
+    f.close()
+res1.sort()
+with open('4.txt', 'w', encoding = 'UTF 8') as file:
+    for i in res1:
+        for key, val in res.items():
+            if val == i:
+                file.write(key + '\n')
+                file.write(str(i) + '\n')
+                with open(key, 'r+', encoding = 'UTF 8') as file_:
+                    a = file_.read()
+                    file.write(a + '\n')
+
+
+
+
+
+
 
